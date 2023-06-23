0e824bb2a0e0de17ee4eddfcfac131a8f0d02e84
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-23 19:22:36 -0300

    style: :art: adds some color transitions on select component

--- src/components/ui/Select/index.tsx
+++ src/components/ui/Select/index.tsx
@@ -9,7 +9,7 @@ interface SelectProps {
 
 function Select({ options }: SelectProps) {
   return (
-    <select className="custom-select h-10 w-full min-w-[192px] rounded-xl border border-black bg-white px-4 text-center text-base font-medium text-black">
+    <select className="custom-select group h-10 w-full min-w-[192px] rounded-xl border border-black bg-white px-4 text-center text-base font-medium text-black transition-all hover:border-primary-blue-300 hover:text-primary-blue-300 disabled:opacity-80 disabled:hover:cursor-not-allowed">
       {options
         .sort((a, b) => a.value - b.value)
         .map((option, index) => (
@@ -17,7 +17,7 @@ function Select({ options }: SelectProps) {
             key={`${index}-${option}`}
             selected={index === 0}
             value={option.value}
-            className="text-start text-base font-medium"
+            className="text-start text-base font-medium group-hover:text-black"
           >
             {option.label}
           </option>
