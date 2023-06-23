b2b9457659c6b54d5f1f8d0a098371502ff3aba0
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-23 19:59:16 -0300

    refactor: :art: creates select options and card mock files

---
+++ src/mocks/cards.ts
@@ -0,0 +1,238 @@
+import thumbnail from "../assets/misc/thumbnail.png";
+
+export const cards = [
+  {
+    id: 1,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 1,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 2,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 3,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 4,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 5,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 6,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 7,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+  {
+    id: 8,
+    thumbnail: thumbnail,
+    video: "https://via.placeholder.com/360x290",
+    title: "Como aumentar sua Geração de Leads feat. Traktor",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nisi eget nunc ultricies aliquam.",
+    assets: [
+      {
+        type: "spreadsheet",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "document",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "presentation",
+        url: "https://via.placeholder.com/360x290",
+      },
+      {
+        type: "folder",
+        url: "https://via.placeholder.com/360x290",
+      },
+    ],
+  },
+];
---
+++ src/mocks/selectOptions.ts
@@ -0,0 +1,5 @@
+export const options = [
+  { value: 1, label: "Data de Publicação" },
+  { value: 2, label: "Nome da Agência" },
+  { value: 3, label: "Nome do Chatbot" },
+];
