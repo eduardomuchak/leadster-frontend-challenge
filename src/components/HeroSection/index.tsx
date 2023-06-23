74cfbb1949c7d3782cfd7363af60188371df19be
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-23 19:16:01 -0300

    feat: :sparkles: adds buttons and select to order cards

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,10 +1,31 @@
 import { Button } from "../ui/Button";
+import Select from "../ui/Select";
 
 function HeroSection() {
+  const options = [
+    { value: 1, label: "Data de Publicação" },
+    { value: 2, label: "Nome da Agência" },
+    { value: 3, label: "Nome do Chatbot" },
+  ];
+
   return (
     <section className="bg-red flex w-full bg-white px-5 py-6 md:py-12 lg:py-[90px]">
       <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center">
-        <Button>Agências</Button>
+        <div className="flex w-full items-center justify-between border-b-2 border-gray-bd pb-6">
+          <div className="flex w-full flex-row items-center gap-3 ">
+            <Button>Agências</Button>
+            <Button>Chatbot</Button>
+            <Button>Marketing Digital</Button>
+            <Button>Geração de Leads</Button>
+            <Button>Mídia Paga</Button>
+          </div>
+          <div className="flex min-w-[312px] flex-row items-center gap-3 ">
+            <label className="flex min-w-[100px] flex-1 text-base font-semibold">
+              Ordenar por
+            </label>
+            <Select options={options} />
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/components/ui/Select/index.tsx
+++ src/components/ui/Select/index.tsx
@@ -10,16 +10,18 @@ interface SelectProps {
 function Select({ options }: SelectProps) {
   return (
     <select className="custom-select h-10 w-full min-w-[192px] rounded-xl border border-black bg-white px-4 text-center text-base font-medium text-black">
-      {options.map((option, index) => (
-        <option
-          key={`${index}-${option}`}
-          selected={index === 0}
-          value={option.value}
-          className="text-start text-base font-medium"
-        >
-          {option.label}
-        </option>
-      ))}
+      {options
+        .sort((a, b) => a.value - b.value)
+        .map((option, index) => (
+          <option
+            key={`${index}-${option}`}
+            selected={index === 0}
+            value={option.value}
+            className="text-start text-base font-medium"
+          >
+            {option.label}
+          </option>
+        ))}
     </select>
   );
 }
