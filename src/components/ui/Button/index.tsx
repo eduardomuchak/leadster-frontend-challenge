927659720bd864f6153f17fe7d423e2562e0df3c
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-23 16:43:44 -0300

    fix: :art: adjusts button text paddings

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -10,7 +10,7 @@ export function Button({ children, ...rest }: ButtonProps) {
       className="group w-fit rounded-full border border-black bg-white text-black transition-colors hover:border-primary-blue-300 disabled:opacity-80 disabled:hover:cursor-not-allowed"
       {...rest}
     >
-      <span className="flex w-fit items-center justify-center px-10 py-3 text-center text-base font-medium leading-5 transition-colors group-hover:text-primary-blue-400">
+      <span className="flex w-fit items-center justify-center px-5 py-2 text-center text-sm font-medium leading-5 transition-colors group-hover:text-primary-blue-400 lg:text-base">
         {children}
       </span>
     </button>
