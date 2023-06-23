37657976243fc06388cd4071ce5a01e098c5f628
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-23 13:31:47 -0300

    feat: :sparkles: creates header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import logo from "@/assets/brand/logo.png";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="flex h-32 w-full items-center justify-center">
+      <Image src={logo} alt="logo" width={200} height={45} />
+    </header>
+  );
+}
+
+export default Header;
