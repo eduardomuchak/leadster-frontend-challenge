5150b2cef92ac26b47c8dd80a32462ab65735c14
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-26 11:48:09 -0300

    feat(wip): :sparkles: add social media icons and conditional render logic on footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,7 @@
 import logo from "@/assets/brand/logo.png";
+import { Facebook, Instagram, LinkedinIcon } from "lucide-react";
 import Image from "next/image";
+import colors from "tailwindcss/colors";
 
 function Footer() {
   const websiteRoadMap = [
@@ -87,6 +89,33 @@ function Footer() {
     },
   ];
 
+  const handleIcon = (title: string) => {
+    switch (title) {
+      case "Facebook":
+        return (
+          <Facebook
+            size={20}
+            color={colors.zinc[400]}
+            strokeWidth={1}
+            fill={colors.zinc[400]}
+          />
+        );
+      case "Linkedin":
+        return (
+          <LinkedinIcon
+            size={20}
+            color={colors.zinc[400]}
+            strokeWidth={1}
+            fill={colors.zinc[400]}
+          />
+        );
+      case "Instagram":
+        return <Instagram size={20} color={colors.zinc[400]} />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <footer className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center py-10">
       <div className="mb-6 flex flex-col gap-1 md:mb-0">
@@ -107,7 +136,7 @@ function Footer() {
                 {item.links.map((link, index) => (
                   <li key={`${index}-${link.title}`}>
                     <a
-                      className="font-medium text-zinc-400 hover:text-zinc-500"
+                      className="text-sm font-medium text-zinc-400 hover:text-zinc-500 lg:text-base"
                       href={link.href}
                     >
                       {link.title}
@@ -119,21 +148,41 @@ function Footer() {
           ) : (
             <div
               key={`${index}-${item.title}`}
-              className="w-full bg-red-500 px-2 py-4 md:py-10"
+              className="w-full px-2 py-4 md:py-10"
             >
               <span className="font-semibold text-zinc-500">{item.title}</span>
-              <ul className="mt-10 flex flex-row gap-5">
+              <ul className="mt-4 flex flex-row flex-wrap gap-2 lg:gap-5">
                 {item.links.map((link, index) => (
                   <li key={`${index}-${link.title}`}>
                     <a
-                      className="flex h-11 w-11 items-center justify-center rounded-full bg-zinc-400 font-medium text-white hover:bg-zinc-500"
+                      className="flex items-center justify-center rounded-full bg-zinc-50 p-3.5 text-zinc-500 hover:bg-zinc-100"
                       href={link.href}
+                      target="_blank"
+                      rel="noreferrer"
                     >
-                      {link.title}
+                      {handleIcon(link.title)}
                     </a>
                   </li>
                 ))}
               </ul>
+              <div className="mt-4 flex flex-col gap-5">
+                <div className="flex flex-col flex-wrap gap-1 md:flex-row">
+                  <span className="text-sm font-medium text-zinc-600 lg:text-base">
+                    E-mail:
+                  </span>
+                  <span className="text-sm font-medium text-zinc-400 lg:text-base">
+                    {item.email}
+                  </span>
+                </div>
+                <div className="flex flex-col flex-wrap gap-1 md:flex-row">
+                  <span className="text-sm font-medium text-zinc-600 lg:text-base">
+                    Telefone:
+                  </span>
+                  <span className="text-sm font-medium text-zinc-400 lg:text-base">
+                    {item.phone}
+                  </span>
+                </div>
+              </div>
             </div>
           )
         )}
