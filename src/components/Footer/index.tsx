772f9ab7afb2fc027f812487e8a4264832e969ab
Eduardo Muchak <eduardomuchak@gmail.com>
2023-06-26 12:59:20 -0300

    refactor: :art: creates a separate file with website roadmap links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,93 +2,9 @@ import logo from "@/assets/brand/logo.png";
 import { Facebook, Instagram, LinkedinIcon } from "lucide-react";
 import Image from "next/image";
 import colors from "tailwindcss/colors";
+import { websiteRoadMap } from "./WebsiteRoadMap";
 
 function Footer() {
-  const websiteRoadMap = [
-    {
-      title: "Links principais",
-      links: [
-        {
-          title: "Home",
-          href: "/",
-        },
-        {
-          title: "Ferramenta",
-          href: "/",
-        },
-        {
-          title: "Preços",
-          href: "/",
-        },
-        {
-          title: "Contato",
-          href: "/",
-        },
-      ],
-    },
-    {
-      title: "Cases",
-      links: [
-        {
-          title: "Geração de Leads B2B",
-          href: "/",
-        },
-        {
-          title: "Geração de Leads em Software",
-          href: "/",
-        },
-        {
-          title: "Geração de Leads em Imobiliária",
-          href: "/",
-        },
-        {
-          title: "Cases de Sucesso",
-          href: "/",
-        },
-      ],
-    },
-    {
-      title: "Materiais",
-      links: [
-        {
-          title: "Blog",
-          href: "/",
-        },
-        {
-          title: "Parceria com Agências",
-          href: "/",
-        },
-        {
-          title: "Guia Definitivo do Marketing",
-          href: "/",
-        },
-        {
-          title: "Materiais Gratuitos",
-          href: "/",
-        },
-      ],
-    },
-    {
-      title: "Siga a Leadster",
-      links: [
-        {
-          title: "Linkedin",
-          href: "https://www.linkedin.com/company/leadster-platform",
-        },
-        {
-          title: "Facebook",
-          href: "https://www.facebook.com/leadsterplatform",
-        },
-        {
-          title: "Instagram",
-          href: "https://www.instagram.com/leadster.com.br",
-        },
-      ],
-      email: "contato@leadster.com.br",
-      phone: "(41) 98828-9851",
-    },
-  ];
-
   const handleIcon = (title: string) => {
     switch (title) {
       case "Facebook":
